519cba450e7072efeece8e7a599a95f760ef0578
Michał Murawski <m.a.murawski@wp.pl>
2023-01-17 00:36:23 +0100

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -4,35 +4,33 @@ import css from './Searchbar.module.css';
 import { useState } from 'react';
 
 export function Searchbar(props) {
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState('');
 
   function handleSubmit(e) {
     e.preventDefault();
     props.searchImages(search);
-  };
-
-    return (
-      <header className={css.searchbar}>
-        <form className={css.form} onSubmit={handleSubmit}>
-          <button type="submit" className={css.button}>
-            <span className={css['button-label']}>Search</span>
-          </button>
-
-          <input
-            className={css.input}
-            name="search"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={search}
-            onChange={e => setSearch(e.currentTarget.value)}
-          />
-        </form>
-      </header>
-    );
   }
 
+  return (
+    <header className={css.searchbar}>
+      <form className={css.form} onSubmit={handleSubmit}>
+        <button type="submit" className={css.button}>
+          <span className={css['button-label']}>Search</span>
+        </button>
+        <input
+          className={css.input}
+          name="search"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={search}
+          onChange={e => setSearch(e.currentTarget.value)}
+        />
+      </form>
+    </header>
+  );
+}
 
 Searchbar.propTypes = {
   searchImages: PropTypes.func.isRequired,
